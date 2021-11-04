87e98f21441254686953bc7071b22d27d692ad66
GabrielSalmazi <73569650+GabrielSalmazi@users.noreply.github.com>
2021-11-04 20:22:27 -0300

    Update Login.jsx

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,7 +1,64 @@
 const Login = function() {
 
     return (
-        <html>Login</html>
+                <!DOCTYPE html>
+        <html>
+
+        <head>
+          <meta charset='utf-8'>
+          <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+          <title>Service Delivery</title>
+          <meta name='viewport' content='width=device-width, initial-scale=1'>
+
+
+
+
+
+          <script src="https://use.fontawesome.com/b1fa1fbd7d.js"></script>
+          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+            integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+
+
+
+          <!-- script -->
+          <script src="https://code.jquery.com/jquery-3.3.1.min.js"
+            integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+          <script src="https://momentjs.com/downloads/moment-with-locales.js"></script>
+          <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+          <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+        </head>
+
+        <body>
+
+          <div class="container" id="bloco">
+            <div class="row d-flex justify-content-center mt-3 w-100">
+              <div class="col-md-6 login" align="center">
+
+              </div>
+              <div class="col-md-6 login" align="center">
+                <img src="ENTREGA (3).png" alt="">
+
+
+                <hr class="mt-5">
+                <button class="btnGoogle"><i class="fa fa-google"></i> Entrar com Google</button>
+                <button class="btnGoogle"><i class="fa fa-envelope"></i> Entrar com Email</button>
+
+
+              </div>
+
+
+            </div>
+
+          </div>
+        </body>
+
+        </html>
     );
 
 }
