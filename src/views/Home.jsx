86d26638b63496242ba3efcc29db3e2e7a6626f8
Matheus Prado Agostinho <mat.matheus_pa@hotmail.com>
2021-11-08 10:14:18 -0300

    project structuring

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,36 +1,5 @@
-import db from "../services/database";
-import { onValue, get, child, ref } from "firebase/database";
-import { useEffect, useState } from "react";
-
-const Home = function () {
-    const [pessoas, setPessoas] = useState([]);
-
-    useEffect(() => {
-        onValue(ref(db, '/Pessoa'), response => {
-            const data = response.val();
-
-            const listPessoas = [];
-            if (data) {
-                for (let id in data) {
-                    listPessoas.push(data[id]);
-                }
-                setPessoas(listPessoas)
-            }
-        })
-    }, []);
-
-    return (
-        <table>
-            {
-                pessoas.map((pessoa, i) => 
-                    <tr key={i}>
-                        <td>{pessoa.nome_completo}</td>
-                    </tr>
-                )
-            }
-        </table>
-    );
-
+export default function Home(){
+  return (
+    <></>
+  )
 }
-
-export default Home;
--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,34 +1,8 @@
-import logo from "../res/logo.png";
 
-const Login = function () {
 
+export default function Login() {
   return (
-    <div>
-      <div className="container" id="bloco">
-        <div className="row d-flex justify-content-center mt-3 w-100">
-          <div className="col-md-6 login" align="center">
-
-          </div>
-          <div className="col-md-6 login" align="center">
-            <img src={logo} alt="logo" />
-
-
-            <hr className="mt-5" />
-            <button className="btnGoogle"><i className="fa fa-google"></i> Entrar com Google</button>
-            <button className="btnGoogle"><i className="fa fa-envelope"></i> Entrar com Email</button>
-
-
-          </div>
-
-
-        </div>
-
-      </div>
-    </div>
-
-
-  );
-
+    <>
+    </>
+  )
 }
-
-export default Login;
