0c2429791a69855c4b502a4142eda97c7701300c
Matheus P Agostinho <mat.matheus_pa@hotmail.com>
2021-11-10 12:51:49 -0400

    starting profile screen

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -3,14 +3,14 @@ import { Route, Switch } from "react-router";
 //Pages
 import Login from "../views/Login";
 import Home from "../views/Home";
-import Page from "../views/Page";
+import Profile from "../views/Profile";
 
 export default function Router() {
   return (
     <Switch>
       <Route exact path="/" component={Login} />
       <Route path="/home" component={Home} />
-      <Route path="/menu" component={Page} />
+      <Route path="/perfil" component={Profile} />
     </Switch>
   );
 }
---
+++ src/views/Profile.jsx
@@ -0,0 +1,54 @@
+import { Menu } from "../components/Menu";
+
+//Componentes Material UI
+import { TextField } from "@material-ui/core";
+
+//Estilos
+import styles from "../styles/profile.module.css";
+
+export default function Profile() {
+  return (
+    <div>
+      <Menu />
+      <div className={styles.app}>
+        <div className={styles.container}>
+          <div className={styles.sides}>
+            <span>Minhas Informações</span>
+            <div>
+              <TextField
+                id="outlined-basic"
+                label="Nome"
+                variant="outlined"
+                size="small"
+              />
+              <TextField
+                id="outlined-basic"
+                label="Sobrenome"
+                variant="outlined"
+                size="small"
+              />
+            </div>
+            <div>
+              <TextField
+                id="outlined-basic"
+                label="CPF"
+                variant="outlined"
+                size="small"
+              />
+              <TextField
+                id="outlined-basic"
+                label="RG"
+                variant="outlined"
+                size="small"
+              />
+            </div>
+          </div>
+
+          <div className={styles.dividerBar}></div>
+
+          <div className={styles.sides}></div>
+        </div>
+      </div>
+    </div>
+  );
+}
