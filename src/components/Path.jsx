57dbc94f659f655ddc1ee0422f6b1931c5020250
Matheus Prado Agostinho <mat.matheus_pa@hotmail.com>
2021-12-01 21:38:00 -0300

    fix problem icon path

--- src/components/Path.jsx
+++ src/components/Path.jsx
@@ -1,41 +1,37 @@
-import { useEffect, useState } from "react";
 import styles from "../styles/components/path.module.css";
 
 //Imagens
 import truckImg from "../res/truck.svg";
 import loadingTruckImg from "../res/loading-truck.svg";
-import flagImg from "../res/flag.svg";
+import grayFlagImg from "../res/grayFlag.svg";
+import greenFlagImg from "../res/greenFlag.svg";
+import blackFlagImg from "../res/flag.svg";
 
 //Componente de trajeto
 export function Path({ value, status }) {
 
-  function chooseIcon() {
-    if (status === "waitingForTransport") {
-      //Aguardando transporte
-      return (
+  return (
+    <div className={styles.container}>
+      {status === "waitingForTransport" ? (
         <img
           className={styles.loadingTruck}
           src={loadingTruckImg}
           alt="CarregandoCaminhão"
         />
-      );
-    } else if (value > 5) {
-      //Em transporte
-      return <div className={styles.square}></div>;
-    }
-  }
-
-  return (
-    <div className={styles.container}>
-      {chooseIcon()}
+      ) : (
+        value > 5 && <div className={styles.square}></div>
+      )}
       <div className={styles.backPath}></div>
       {true && (
         <div className={styles.traject} style={{ width: `${value}%`, }}>
           <div className={styles.pathInProgress}></div>
-          <img className={styles.truck} src={truckImg} alt="Caminhão" />
+          {status === "onCarriage" && <img className={styles.truck} src={truckImg} alt="Caminhão" />}
         </div>
       )}
-      <img className={styles.flag} src={flagImg} alt="Bandeira" />
+      {status === "waitingForTransport" ? <img className={styles.flag} src={blackFlagImg} alt="Bandeira" />
+        : status === "onCarriage" ? <img className={styles.flag} src={grayFlagImg} alt="Bandeira" />
+          : <img className={styles.flag} src={greenFlagImg} alt="Bandeira" />
+      }
     </div>
   );
 }
